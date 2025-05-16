fb279d2cb88225953af1094890b0945415796dca
Danalache Sebastian <sebastian.danalache@student.tuiasi.ro>
2025-05-16 18:05:09 +0300

    Add project files.

---
+++ MagentoTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading;
+using AC2025.TestData;
+
+namespace AC2025
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Support.UI;
+    using SeleniumExtras.WaitHelpers;
+
+    [TestClass]
+    public class MagentoTests
+    {
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.opencart.com/");
+        }
+
+        [TestMethod]
+        public void HomePage_Title_IsCorrect()
+        {
+            // Verifică dacă titlul paginii principale este cel așteptat
+            Assert.AreEqual("OpenCart - Open Source Shopping Cart Solution", driver.Title);
+        }
+
+
+        [TestMethod]
+        public void Navigate_To_Features_Page()
+        {
+            // Navighează la pagina Features și verifică titlul
+            driver.FindElement(By.LinkText("Features")).Click();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.TitleContains("Features"));
+            Assert.IsTrue(driver.Title.Contains("Features"), "Pagina Features nu s-a deschis corect.");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+    }
+
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AC2025")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Centric")]
+[assembly: AssemblyProduct("AC2025")]
+[assembly: AssemblyCopyright("Copyright © Centric 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6b6c2704-ada8-4302-be07-41a3512870f2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
